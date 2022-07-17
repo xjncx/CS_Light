0780652092370ea9289bba3c50a9d73d86dd4654
xjncx <joncy4mm@gmail.com>
2022-07-17 15:30:06 +0300

    added ht

--- OOP War/Program.cs
+++ OOP War/Program.cs
@@ -8,8 +8,172 @@ namespace OOP_War
 {
     class Program
     {
-        static void Main(string[] args)
+        static void Main()
         {
+            List<Soldier> orcsSoldiers = new List<Soldier>();
+            List<Soldier> humanSoldiers = new List<Soldier>();
+            Squad orcsArmy = new Squad(orcsSoldiers, "Орки");
+            Squad humanArmy = new Squad(humanSoldiers, "Люди");
+            orcsArmy.Assemble(5);
+            humanArmy.Assemble(5);
+            orcsArmy.ShowStats();
+            humanArmy.ShowStats();
+            BattleField arena = new BattleField(orcsArmy, humanArmy);
+            arena.Battle();
+        }
+    }
+
+    class Soldier
+    {
+        public int Health { get; private set; }
+        public int Damage { get; private set; }
+
+        public Soldier(int health, int damage)
+        {
+            Health = health;
+            Damage = damage;
+        }
+
+        public Soldier Fill()
+        {
+            Soldier recruit = new Soldier(0, 0);
+            Random random = new Random();
+            int soldierMinHP = 3;
+            int soldierMaxHP = 8;
+            int soldierMinDamage = 1;
+            int soldierMaxDamage = 4;
+            recruit.Health = random.Next(soldierMinHP, soldierMaxHP);
+            recruit.Damage = random.Next(soldierMinDamage, soldierMaxDamage);
+            return recruit;
+        }
+
+        public void ShowInfo()
+        {
+            Console.WriteLine($"Здоровье - {Health}, Урон - {Damage}");
+        }
+
+        public void TakeDamage(int damage)
+        {
+            Health -= damage;
+        }
+    }
+
+    class Squad
+    {
+        private List<Soldier> _soldiers;
+        private Soldier _recruit = new Soldier(0, 0);
+        public string Name { get; private set; }
+        public Squad(List<Soldier> soldiers, string name)
+        {
+            _soldiers = soldiers;
+            Name = name;
+        }
+
+        public void Assemble(int squadCount)
+        {
+            for (int i = 0; i < squadCount; i++)
+            {
+                _soldiers.Add(_recruit.Fill());
+            }
+        }
+
+        public void ShowStats()
+        {
+            Console.WriteLine(Name);
+            foreach (Soldier soldier in _soldiers)
+            {
+                soldier.ShowInfo();
+            }
+        }
+
+        public int GetSoldiersNumber()
+        {
+            return _soldiers.Count;
+        }
+
+        public Soldier SendToBattle(int number)
+        {
+            Soldier soldier;
+            soldier = _soldiers[number];
+            return soldier;
+        }
+
+        public void SendToHospital(int soldierNumber)
+        {
+            _soldiers.RemoveAt(soldierNumber);
+        }
+
+
+    }
+
+    class BattleField
+    {
+        private Squad _leftArmy;
+        private Squad _rightArmy;
+
+        public BattleField(Squad leftArmy, Squad rightArmy)
+        {
+            _leftArmy = leftArmy;
+            _rightArmy = rightArmy;
+        }
+
+        public void Battle()
+        {
+
+            int leftSoldiersCounter = 0;
+            int rightSoldiersCounter = 0;
+            Soldier leftSoldier = _leftArmy.SendToBattle(leftSoldiersCounter);
+            Soldier rightSoldier = _rightArmy.SendToBattle(rightSoldiersCounter);
+
+            while (_leftArmy.GetSoldiersNumber() > 0 && _rightArmy.GetSoldiersNumber() > 0)
+            {
+                leftSoldier.TakeDamage(rightSoldier.Damage);
+
+                if (leftSoldier.Health <= 0)
+                {
+                    _leftArmy.SendToHospital(leftSoldiersCounter);
+                    Console.WriteLine("Солдата левого взвода убили!");
+                    leftSoldiersCounter++;
+                    if (leftSoldiersCounter < _leftArmy.GetSoldiersNumber())
+                    {
+                        leftSoldier = _leftArmy.SendToBattle(leftSoldiersCounter);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Солдаты кончились в левом взводе!");
+                    }
+                }
+
+                rightSoldier.TakeDamage(leftSoldier.Damage);
+
+                if (rightSoldier.Health <= 0)
+                {
+                    _rightArmy.SendToHospital(rightSoldiersCounter);
+                    Console.WriteLine("Солдата правого взвода убили!");
+                    rightSoldiersCounter++;
+                    if (rightSoldiersCounter < _rightArmy.GetSoldiersNumber())
+                    {
+                        rightSoldier = _rightArmy.SendToBattle(rightSoldiersCounter);
+                    }
+                    else
+                    {
+                        Console.WriteLine("Солдаты кончились в левом взводе!");
+                    }
+                }
+            }
+            AnnounceWinner(_leftArmy, _rightArmy);
+        }
+
+        public void AnnounceWinner(Squad left, Squad right)
+        {
+            if (left.GetSoldiersNumber() <= 0)
+            {
+                Console.WriteLine("Выиграли правые!");
+            }
+            else
+            {
+                Console.WriteLine("Выиграли левые!");
+            }
         }
     }
 }
